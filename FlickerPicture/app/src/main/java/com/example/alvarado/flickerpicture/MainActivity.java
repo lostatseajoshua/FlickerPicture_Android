d2289aef88860fb54411133e79320d551af42c9c
Joshua Alvarado <alvaradojoshua0@gmail.com>
2016-11-02 22:08:32 -0600

    Add imports for grid view

--- FlickerPicture/app/src/main/java/com/example/alvarado/flickerpicture/MainActivity.java
+++ FlickerPicture/app/src/main/java/com/example/alvarado/flickerpicture/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.alvarado.flickerpicture;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends AppCompatActivity {
 
